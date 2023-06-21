7c52a8ede811b5595cb31a410763b694893fa8a6
Mohamed Suliman <sulimmoh@gmail.com>
2023-06-21 18:11:57 -0400

    0x03-queuing_system_in_js

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,24 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const object = {
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
+};
+
+const queueName = 'push_notification_code';
+
+const job = queue
+  .create(queueName, object)
+  .save((error) => {
+    if (!error) console.log(`Notification job created: ${job.id}`);
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
